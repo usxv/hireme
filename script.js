44d8498bbf1f73a26e632b82fcc26be2050b4e71
Sumit Verma <138086515+usxv@users.noreply.github.com>
2024-06-23 01:43:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+    function splashScreen() {
+setTimeout(loaded, 2200);
+    }
+    function loaded (){
+        var load= document.getElementById('globe');
+        load.removeAttribute("id");
+        // load.removeAttribute("class");
+        setTimeout(loaded2, 200);
+    }
+  function loaded2(){
+    let x = document.getElementById('splash');
+    x.style.display= "none";
+  }
+</script>
